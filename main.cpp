4f06e6aafe6920ab5a00866502d0a1e4cf9a3516
repl.it user <replituser@example.com>
2021-01-31 14:40:39 +0000

    Crando el archivo main

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+Name:
+Author: Juan Sebastian Guerrero Peña.
+Date: 
+*/
+
+#include <iostream> // Escribir en consola
+#include <fstream>  //Leer y escribir archivos de texto
+#include <stdio.h> //Libreria basica
+#include <string>   // Uso de variables tipo string
+#include <iomanip>  //Para tabular tablas
+using namespace std;  //Reducir la sintaxis de iostream
+
+
+
+int main() {
+
+  cout<<"Hola"<<right;
+  
+}
